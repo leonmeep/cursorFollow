b3b037544ae0f22e22c9692e83721b79d6ce8c52
Leon Birch <leonbirch333@gmail.com>
2024-06-11 14:50:57 +0100

    created css file and js file, coded cursor movement

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const customTracker = document.querySelector(".customTracker");
+
+const pos = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+
+const mouse = { x: pos.x, y: pos.y };
+
+const speed = 0.35;
